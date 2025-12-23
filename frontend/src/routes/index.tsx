4fe836aca3c41aab699f378ef553508bb246a761
Muhammad Ali <ali.shoaib@paysyslabs.com>
2025-12-23 16:26:00 +0500

    feat: Update routing structure to redirect to login

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,14 +1,21 @@
 import { lazy } from "react";
+import { Navigate } from "react-router-dom";
 
 const Components = lazy(() => import("../components"));
 const Login = lazy(() => import("../pages/Auth/Login"));
 
 export const ROUTES = [
     {
-        path: "/",
+        path: "/components",
         element: <Components />,
         private: true,
         layout: false
+    },
+    {
+        path: "/",
+        element: <Navigate to="/login" replace />,
+        private: false,
+        layout: false,
     },
     {
         path: "/login",
